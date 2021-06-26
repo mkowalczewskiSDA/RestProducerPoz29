d9f0f0406ea05e67c297841fce50b6dc10dd814e
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-06-26 12:41:51 +0200

    user

--- src/main/java/com/example/RestProducer/model/User.java
+++ src/main/java/com/example/RestProducer/model/User.java
@@ -1,4 +1,31 @@
 package com.example.RestProducer.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+@Data
+@Entity
+@Table
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+    @Column
+    @NotNull
+    private String firstName;
+    @NotNull
+    @Column
+    private String lastName;
+    @Column
+    @NotNull
+    @Email
+    private String email;
+    @OneToMany(mappedBy = "user")
+    private Set<Task> tasks;
 }
